ed4733d590c230f9c835cb43319f4696cc6b469d
Alpscloud <kuptsov@clouds.st>
2017-10-05 16:19:52 +0300

    my starting template

---
+++ gulpfile.js
@@ -0,0 +1,177 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	concatCss = require('gulp-concat-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssmin = require('gulp-cssmin'),
+	stylus = require('gulp-stylus'),
+	watch = require('gulp-watch'),
+	rename = require('gulp-rename'),
+	imagemin = require('gulp-imagemin'),
+	imageminJpegRecompress = require('imagemin-jpeg-recompress'),
+	pngquant = require('imagemin-pngquant'),
+	uglify = require('gulp-uglify'),
+	cache = require('gulp-cache'),
+	debug = require('gulp-debug'),
+	browserSync = require('browser-sync').create();
+
+
+
+// ========= ========= T A S K S ========= =========
+
+//Compile Stylus files to min.css + vendor + uncss
+gulp.task('styl', function() {
+	return gulp.src('./src/styl/main.styl')
+		.pipe(debug({title:'SRC'}))
+		.pipe(stylus({linenos: false}))
+			.on('error', function(err) {
+				console.log(err.message);
+				this.end();
+			})
+		.pipe(debug({title:'STYLUS'}))
+		.pipe(autoprefixer([
+			'Android 2.3',
+			'Android >= 4',
+			'Chrome >= 20',
+			'Firefox >= 24',
+			'Explorer >= 8',
+			'iOS >= 6',
+			'Opera >= 12',
+			'Safari >= 6'
+		]))
+		.pipe(debug({title:'AUTOPREFIXER'}))
+		.pipe(cssmin())
+		.pipe(debug({title:'CSSMIN'}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(debug({title:'RENAME'}))
+		.pipe(gulp.dest('./src/css/'))
+		.pipe(debug({title:'DESTINATION'}))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+
+
+// Concat + compress + rename css LIBS files
+gulp.task('cssLibs', function () {
+	return gulp.src('./src/css/libs/*.css')
+		.pipe(concatCss('libs.css'))
+		.pipe(cssmin())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./src/css/'))
+		.pipe(browserSync.reload({stream: true}));
+})
+
+// Concat + compress +rename js files
+gulp.task('jsLibs', function () {
+    return gulp.src('./src/js/libs/*.js')
+    	.pipe(concat('libs.js'))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./src/js/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// Common js file
+gulp.task('jsCommon', function () {
+	return gulp.src('./src/js/common.js')
+		.pipe(uglify())
+			.on('error', function(err) {
+				console.log(err.message);
+				this.end();
+			})
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./src/js'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+// Concat all libs css files and libs js files
+gulp.task('buildLibs', ['cssLibs', 'jsLibs']);
+
+
+// Browsersync
+gulp.task('serve', function() {
+	browserSync.init({ 
+		server: { 
+			baseDir: "src" 
+		}, 
+		notify: false
+		// online: true, 
+		// tunnel: true, 
+		// tunnel: "projectmane" //Demonstration page: http://projectmane.localtunnel.me 
+	}); 
+	
+});
+
+// Watch task
+gulp.task('watch', ['serve'], function() {
+	gulp.watch('./src/*.html', browserSync.reload);
+	gulp.watch("./src/styl/**/*.styl", ['styl']);
+	gulp.watch("./src/js/common.js", ['jsCommon']);
+	gulp.watch('./src/css/libs/*.css', ['cssLibs']);
+});
+
+	
+
+// ========= ========= ========= ========= ========= 
+
+// ========= ========= B U I L D   O F   T H E   P R O J E C T  ========= =========
+
+// Moves html file from src folder to APP folder
+gulp.task('htmlBuild', function () {
+	return gulp.src('./src/*.html')
+		.pipe(gulp.dest('./app/'));
+});
+
+// Moves main css file from src/CSS folder to APP/CSS folder
+gulp.task('cssBuild', function () {
+	return gulp.src('./src/css/**/*')
+		.pipe(gulp.dest('./app/css/'));
+});
+
+// Moves js files from src/JS folder to APP/JS folder
+gulp.task('jsBuild', function () {
+	return gulp.src('./src/js/**/*.min.js')
+		.pipe(gulp.dest('./app/js/'));
+});
+
+// Moves + 'minificate' images from src/IMG folder to APP/IMG
+gulp.task('imgBuild', function () {
+	return gulp.src('./src/img/**/*')
+		.pipe(cache(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imageminJpegRecompress({
+			loops: 5,
+			min: 65,
+			max: 70,
+			quality:'medium'
+		}),
+		imagemin.svgo(),
+		imagemin.optipng({optimizationLevel: 3}),
+		pngquant({quality: '65-70', speed: 5})
+		],{
+		verbose: true
+		})))
+		.pipe(gulp.dest('./app/img/'));
+});
+
+// Clearing the cache
+gulp.task('clear', function (done) {
+	return cache.clearAll(done);
+});
+
+gulp.task('fontsBuild', function() {
+	return gulp.src('./src/fonts/**/*')
+		.pipe(gulp.dest('./app/fonts/'));
+});
+
+// Start of building
+gulp.task('build', ['htmlBuild', 'jsBuild', 'cssBuild','imageBuild', 'fontsBuild']);
+// ========= ========= ========= ========= ========= 
